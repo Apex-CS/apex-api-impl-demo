46aeb302211266a1bc6d9db00a3e1b024d4bde94
avaldezintersys <avaldez@intersysconsulting.com>
2020-01-30 15:39:08 -0600

    Add jsd2xml tool, TODO: add as maven task the build of json -> xml

--- integrations/src/main/java/com/fis/api/product/BPPRetrieveProductDetail/jsd2xml.java
+++ integrations/src/main/java/com/fis/api/product/BPPRetrieveProductDetail/jsd2xml.java
@@ -2,13 +2,11 @@ package com.fis.api.product.BPPRetrieveProductDetail;
 
 import com.ethlo.jsons2xsd.Config;
 import com.ethlo.jsons2xsd.Jsons2Xsd;
+import com.ethlo.jsons2xsd.XmlUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,63 +15,69 @@ import java.util.stream.Stream;
 
 public class jsd2xml {
 
+    private static Logger logger =  LoggerFactory.getLogger(jsd2xml.class);
+
     public static void main(String[] args) throws IOException {
 
-        System.out.println(args);
-
         if(args.length == 1){
-            System.out.println("running convert for: "+ args[0]);
+            logger.info("running convert for: "+ args[0]);
             execute(args[0]);
         }
         else{
-            System.out.println("args " + args.length);
+            logger.info("args " + args.length);
         }
     }
 
-    public static void execute(String path) throws IOException {
-        Stream<Path> paths = Files.walk(Paths.get(path));
+    public static void execute(String pathFile) throws IOException {
+        Stream<Path> paths = Files.walk(Paths.get(pathFile));
 
         paths.filter(Files::isRegularFile)
-                /*.filter(fileName -> fileName.endsWith(".json"))*/
-                .forEach(file -> {
-                    System.out.println("converting > " + file.toFile().getName());
+                .filter( f -> f.toFile().getName().endsWith("json") )
+                .forEach(path -> {
+                    logger.info("converting > " + path.toFile().getName());
                     try {
-                        Document doc = convert(file.toFile());
-                        Dom2File(doc, new File(getXSDName(file.toFile().getName())));
-                    } catch (IOException | TransformerException e ) {
+                        String doc = convert(path.toFile());
+                        // logger.info(doc);
+                        saveXML( getXSDPathName(path) + getXSDFileName(path)
+                                ,doc);
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
                 });
-    }
-
-    public static void Dom2File(Document document, File file) throws TransformerException, IOException {
-        DOMSource source = new DOMSource(document);
-        FileWriter writer = new FileWriter(file);
-        StreamResult result = new StreamResult(writer);
-
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-        Transformer transformer = transformerFactory.newTransformer();
-        transformer.transform(source, result);
- 
     }
 
     public static String getEntityName(String fileName){
         return fileName.replace(".json", "");
     }
 
-    public static String getXSDName(String fileName){
+    public static String getXSDPathName(Path path){
+        String pathName = path.toFile().getParentFile().getAbsolutePath();
+        return pathName.replace("json", "xml\\");
+    }
+
+    public static String getXSDFileName(Path path){
+        String fileName = path.toFile().getName();
         return fileName.replace(".json", ".xml");
     }
 
-    public static Document convert(File file) throws IOException {
+    public static void saveXML(String fileName, String content) throws IOException {
+        FileWriter fileWriter = new FileWriter(new File(fileName));
+        logger.info("saving file > "+ fileName);
+        fileWriter.write(content);
+        fileWriter.flush();
+        fileWriter.close();
+    }
+
+    public static String convert(File file) throws IOException {
         String entity = getEntityName(file.getName());
         Reader reader = new FileReader(file);
         Config cfg = new Config.Builder()
                 .targetNamespace("http://example.com/myschema.xsd")
-                .name( "array" )
+                .name( "object" )
                 .build();
         Document doc = Jsons2Xsd.convert(reader, cfg);
-        return doc;
+        final String actual = XmlUtil.asXmlString(doc.getDocumentElement());
+        return actual;
     }
 
 
